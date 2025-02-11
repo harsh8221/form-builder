6c49e24dd988dffed27ec0f2076decee3a247378
Harsh Patel <harsh8221@gmail.com>
2025-02-11 23:24:02 +0530

    Home page button added

--- src/components/templates/Home/Home.tsx
+++ src/components/templates/Home/Home.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+
+import { useNavigate } from 'react-router-dom';
+import { Button, Typography } from '@mui/material';
+
+import style from './style.module.css';
 
 const Home: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className={style.container}>
+      <Typography className={style.title} variant='h4'>
+        Home Page
+      </Typography>
+      <div className={style.buttons}>
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={() => navigate('/form')}>
+          Fill the Form
+        </Button>
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={() => navigate('/form-builder')}>
+          Create a Form
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
