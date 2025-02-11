5435880544683fb629c3c8e357dbe625358d13fa
Harsh Patel <harsh8221@gmail.com>
2025-02-12 02:13:04 +0530

    Implemented: No Question View on Form

--- src/components/templates/Form/Form.tsx
+++ src/components/templates/Form/Form.tsx
@@ -1,14 +1,21 @@
 import React, { useEffect } from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
+import { Button, Typography } from '@mui/material';
+
 import { Question } from '@interface/Question';
 
 import FormUI from '@components/organisms/FormUI';
 
 import { getFormQuestions } from './form.helper';
 import style from './style.module.css';
+import NoQuestionView from '@components/molecules/NoQuestionView';
 
 const Form: React.FC = () => {
   const [form, setForm] = React.useState<Question[]>([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     getFormQuestions(setForm);
@@ -16,8 +23,19 @@ const Form: React.FC = () => {
 
   return (
     <div className={style.container}>
-      Fill the Form
-      <FormUI form={form} />
+      <Typography className={style.titleContainer} variant='h4'>
+        Fill the Form
+      </Typography>
+      {Object.entries(form).length === 0 && <NoQuestionView />}
+      {Object.entries(form).length === 0 && (
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={() => navigate('/form-builder')}>
+          Create a Form
+        </Button>
+      )}
+      {Object.entries(form).length !== 0 && <FormUI form={form} />}
     </div>
   );
 };
